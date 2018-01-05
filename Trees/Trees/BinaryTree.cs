f3bafcaf9f5e91663d4d9e8e4a634500eece1f38
killemil <esend3@yahoo.com>
2018-01-05 12:48:21 +0200

    Trees

---
+++ Trees/Trees/BinaryTree.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    private T value;
+    private BinaryTree<T> leftChild;
+    private BinaryTree<T> rightChild;
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.LeftChild = leftChild;
+        this.RightChild = rightChild;
+    }
+
+    public T Value
+    {
+        get
+        {
+            return this.value;
+        }
+        set
+        {
+            this.value = value;
+        }
+    }
+
+    public BinaryTree<T> LeftChild
+    {
+        get
+        {
+            return this.leftChild;
+        }
+        set
+        {
+            this.leftChild = value;
+        }
+    }
+
+    public BinaryTree<T> RightChild
+    {
+        get
+        {
+            return this.rightChild;
+        }
+        set
+        {
+            this.rightChild = value;
+        }
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + this.Value);
+
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(indent + 2);
+        }
+
+        if (this.LeftChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(indent + 2);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this, action);
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        this.EachPostOrder(this, action);
+    }
+
+    private void EachInOrder(BinaryTree<T> current, Action<T> action)
+    {
+        if (current == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(current.LeftChild, action);
+        action(current.Value);
+        this.EachInOrder(current.RightChild, action);
+    }
+
+    private void EachPostOrder(BinaryTree<T> current, Action<T> action)
+    {
+        if (current == null)
+        {
+            return;
+        }
+
+        this.EachPostOrder(current.LeftChild, action);
+        this.EachPostOrder(current.RightChild, action);
+        action(current.Value);
+    }
+}
---
+++ Trees/Trees/Tree.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    private T value;
+    private IList<Tree<T>> children;
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = children;
+    }
+
+    public T Value
+    {
+        get
+        {
+            return this.value;
+        }
+        set
+        {
+            this.value = value;
+        }
+    }
+
+    public IList<Tree<T>> Children
+    {
+        get
+        {
+            return this.children;
+        }
+        set
+        {
+            this.children = value;
+        }
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + this.value);
+
+        foreach (var child in this.children)
+        {
+            child.Print(indent + 2);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.value);
+
+        foreach (var child in this.children)
+        {
+            child.Each(action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        var result = new List<T>();
+
+        this.DFS(this, result);
+
+        return result;
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        var result = new List<T>();
+
+        var queue = new Queue<Tree<T>>();
+        queue.Enqueue(this);
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            result.Add(current.value);
+            foreach (var child in current.children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+
+        return result;
+    }
+
+    private void DFS(Tree<T> tree, List<T> result)
+    {
+        foreach (var child in tree.children)
+        {
+            this.DFS(child, result);
+        }
+
+        result.Add(tree.value);
+    }
+}
